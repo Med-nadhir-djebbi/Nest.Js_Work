077250c30b63a3f3aeaf68203c48f0361ec50028
Mohamed Nadhir Djebbi <mohamednadhirdjebbi@gmail.com>
2026-04-11 00:42:47 +0100

    noth

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,5 +1,9 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Cv } from '../../cv/entities/cv.entity';
+export enum UserRole {
+  USER = 'user',
+  ADMIN = 'admin',
+}
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
