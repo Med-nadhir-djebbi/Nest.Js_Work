e736feba1741ca7fa97126b59d341b6c5e112979
BitravenS <malektabbabi77@gmail.com>
2026-04-11 15:15:40 +0100

    Cleaned up repo and minor bug fixes

--- dist/skill/dto/create-skill.dto.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class CreateSkillDto {
-    designation: string;
-}
--- dist/skill/entities/skill.entity.d.ts
+++
@@ -1,6 +0,0 @@
-import { Cv } from '../../cv/entities/cv.entity';
-export declare class Skill {
-    id: number;
-    designation: string;
-    cvs: Cv[];
-}
