c1ff4999f8a7d0cbf66023d05602e1072eeda1b4
Mohamed Nadhir Djebbi <mohamednadhirdjebbi@gmail.com>
2026-05-05 11:36:55 +0100

    n

--- dist/user/entities/user.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Cv } from '../../cv/entities/cv.entity';
-export declare class User {
-    id: number;
-    username: string;
-    password: string;
-    email: string;
-    role: string;
-    cvs: Cv[];
-}
